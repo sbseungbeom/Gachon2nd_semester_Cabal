23e45a456098550c5405a813e9808c24bc0f393a
화베(조유찬) <81742754+whdbcks111@users.noreply.github.com>
2023-10-17 14:33:14 +0900

    Aim accuracy increased

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _aimDistance;
     [SerializeField] private Projectile _projectilePrefab;
+    [SerializeField] private LineRenderer _aimLine;
+
+    private Vector3[] _positions = new Vector3[2];
 
     private void Update()
     {
@@ -15,13 +19,23 @@ public class Player : MonoBehaviour
 
     private void ShootUpdate()
     {
-        if(Input.GetMouseButtonDown(0))
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var point = ray.GetPoint(_aimDistance);
+        point.y = transform.position.y;
+
+        if(ray.origin.y > transform.position.y && ray.direction.y < 0f)
         {
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            var point = ray.GetPoint(50);
-            if (Physics.Raycast(ray, out var hit))
-                point = hit.point;
-            point.y = transform.position.y;
+            var distance = (ray.origin.y - transform.position.y) / -ray.direction.y;
+            point = ray.GetPoint(distance);
+        }
+
+        _positions[0] = transform.position;
+        _positions[1] = transform.position + (point - transform.position) * 200;
+        _aimLine.SetPositions(_positions);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(_projectilePrefab, point, Quaternion.identity).speed = 0;
             var projectile = Instantiate(_projectilePrefab, transform.position, Quaternion.identity);
             projectile.transform.LookAt(point);
         }
