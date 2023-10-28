d31a2ca1dbaf02ce404796ced4ce07ead058e7ec
sbseungbeom <sbseungbeom99@gmail.com>
2023-10-28 21:22:28 +0900

    SpreadAttack Added.
    잡몹 산탄 공격 구현함.
    Spread : 총알이 퍼지는 정도. 5 권장.
    AmountOfBullet : 생성될 총알 수.
    SpreadCooltime : 총알 생성 주기. 0.05권장,

--- Assets/Scripts/LimSeungBeom/SmallMob_1.cs
+++ Assets/Scripts/LimSeungBeom/SmallMob_1.cs
@@ -18,6 +18,14 @@ public class SmallMob_1 : MonoBehaviour
     [SerializeField] GameObject _bulletPrefab;
     [SerializeField] public GameObject _player;
 
+
+    [Header("산탄 공격이 퍼지는 정도. 5 권장")]
+    [SerializeField] public float Spread;
+    [Header("산탄 공격에서 생성될 총알 수")]
+    [SerializeField] public int _AmountOfBullet;
+    [Header("산탄 공격 총알 생성 주기. 0.05 권장")]
+    [SerializeField] public float _SpreadCooltime;
+
     private int _dir = 1;
     // Start is called before the first frame update
     void Start()
@@ -25,12 +33,6 @@ public class SmallMob_1 : MonoBehaviour
         StartCoroutine(CoolDown());
         StartCoroutine(DirectionChange());
         //_player = GameObject.FindGameObjectWithTag("Player");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
     private void FixedUpdate()
     {
@@ -56,14 +58,18 @@ public class SmallMob_1 : MonoBehaviour
         Vector3 viewPos = Camera.main.WorldToViewportPoint(this.transform.position);
         //만약 적 오브젝트의 크기 문제로 버그 발생시 스프라이트 랜더러의 bounds 값을 받아오는 코드로 교정
         if (viewPos.x >= 0 && viewPos.x <= 1 && viewPos.y >= 0 && viewPos.y <= 1 && viewPos.z > 0)
-            FireBullet();
+            StartCoroutine(FireBullet());
         else
             StartCoroutine(CoolDown());
     }
-    protected void FireBullet()
+    IEnumerator FireBullet()
     {
-        GameObject copy = Instantiate(_bulletPrefab, transform.position, transform.rotation);
-        copy.transform.LookAt(_player.transform.position);
+        for(int i = 0; i < _AmountOfBullet; i++)
+        {
+            GameObject copy = Instantiate(_bulletPrefab, transform.position, transform.rotation);
+            copy.transform.LookAt(new Vector3(_player.transform.position.x - Spread + i * (Spread * 2 / _AmountOfBullet) ,_player.transform.position.y,_player.transform.position.z));
+            yield return new WaitForSeconds(_SpreadCooltime);
+        }
         StartCoroutine(CoolDown());
     }
     IEnumerator CoolDown()
