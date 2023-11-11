c1cc1f94ab6591cd11e27ec17abb6da83b8f5cf5
sbseungbeom <sbseungbeom99@gmail.com>
2023-11-11 16:58:13 +0900

    산탄공격 스크립트 재작성, 상속받지 않게 함.

--- Assets/Scripts/LimSeungBeom/SmallMob_1.cs
+++ Assets/Scripts/LimSeungBeom/SmallMob_1.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
-public class SmallMob_1 : Mob
+public class SmallMob_1 : MonoBehaviour
 {
+
+    [SerializeField] int Direction = -1;
+
+    [SerializeField] float MoveDistance;
+    [SerializeField] float MinMoveDistance;
+    [SerializeField] float MaxMoveDistance;
+
+    [SerializeField] float MoveSpeed;
+    [SerializeField] float MoveWaitTime;
+    [SerializeField] float MinMoveWaitTime;
+    [SerializeField] float MaxMoveWaitTime;
+
+    float AttackWaitTime;
+    [SerializeField] float MaxAttackWaitTime;
+    [SerializeField] float MiNAttackWaitTime;
+
     [Header("산탄 공격이 퍼지는 정도. 5 권장")]
     [SerializeField] public float Spread;
     [Header("산탄 공격에서 생성될 총알 수")]
@@ -11,28 +28,58 @@ public class SmallMob_1 : Mob
     [Header("산탄 공격 총알 생성 주기. 0.05 권장")]
     [SerializeField] public float _SpreadCooltime;
 
-    // Start is called before the first frame update
-    protected override void Start()
+    GameObject Player;
+    [SerializeField] GameObject Projectile;
+
+    void Start()
     {
-        base.Start();
+        Player = GameObject.FindWithTag("Player");
+        StartCoroutine(MobMove());
+        StartCoroutine(Attack());
+
     }
-    protected override void FixedUpdate()
+    private void Update()
     {
-        base.FixedUpdate();
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(1 * Direction * MoveDistance, transform.position.y, transform.position.z), Time.deltaTime * MoveSpeed);
     }
-    protected override void FireBullet()
+    IEnumerator MobMove()
     {
-        //base.FireBullet();
-        StartCoroutine(FireBulletCoroutine());
-    }
-    IEnumerator FireBulletCoroutine()
-    {
-        for (int i = 0; i < _AmountOfBullet; i++)
+        //------------------ 방향 제어--------------
+        Vector3 viewpos = Camera.main.WorldToViewportPoint(this.gameObject.transform.position); // 카메라 왼쪽 밖으로 나와있는지 확인
+        if (viewpos.x <= 0) { Direction = 1; }
+        if (viewpos.x >= 1) { Direction = -1; }
+        if (0 < viewpos.x && viewpos.x < 1)
         {
-            GameObject copy = Instantiate(_bulletPrefab.gameObject, transform.position, transform.rotation);
-            copy.GetComponent<EnemyProjectile>().SetTarget(_player.transform.position);
+            Direction = Random.Range(-1, 1);
+            if (Direction == 0) Direction = 1;
+        }
+
+        MoveDistance = Random.Range(MinMoveDistance, MaxMoveDistance + 1); // 이동 거리
+        MoveWaitTime = Random.Range(MinMoveWaitTime, MaxMoveWaitTime + 1); // 이동 후 다음이동까지 대기시간
+        Direction = Random.Range(-1, 1);
+        if (Direction == 0) Direction = 1;
+
+
+        yield return new WaitForSeconds(MoveWaitTime);                          //전부 기다렸으면 다음 이동 시작
+        StartCoroutine(MobMove());
+    }
+
+    IEnumerator Attack()
+    {
+        AttackWaitTime = Random.Range(MiNAttackWaitTime,MaxAttackWaitTime + 1);
+        Vector3 SettedPlayerPosition = Player.transform.position;
+
+        for(int i = 0; i < _AmountOfBullet;  i++)
+        {
+            GameObject bullet = Instantiate(Projectile, transform.position, Quaternion.identity);
+            bullet.transform.LookAt(new Vector3((SettedPlayerPosition.x - Spread) + (i * (Spread * 2 / _AmountOfBullet)), SettedPlayerPosition.y, SettedPlayerPosition.z));
             yield return new WaitForSeconds(_SpreadCooltime);
         }
-        StartCoroutine(CoolDown());
+
+
+
+        yield return new WaitForSeconds(AttackWaitTime);
+        StartCoroutine (Attack());
     }
+
 }
