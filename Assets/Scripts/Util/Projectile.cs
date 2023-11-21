c1491c3203ee3d708e1217a5ee02b8fe387d256a
화베(조유찬) <81742754+whdbcks111@users.noreply.github.com>
2023-11-21 17:08:44 +0900

    오브 모델링 변경, 보스 카메라 움직임 구현

--- Assets/Scripts/Util/Projectile.cs
+++ Assets/Scripts/Util/Projectile.cs
@@ -10,12 +10,19 @@ public class Projectile : MonoBehaviour
     public float DestroyDistance = 50f;
     public float DistanceCheckTime = 1f;
 
-    private float _distanceCheckTimer = 0f;
-
+    [HideInInspector] public Vector3 CameraOffset;
+    
 
     void Update ()
     {
         transform.position += Time.deltaTime * speed * transform.forward;
+
+        var movement = Time.deltaTime * 2f;
+
+        transform.position += -CameraOffset * movement;
+        CameraOffset *= (1 - movement);
+
+        if(!IsEnemyProjectile) print(CameraOffset);
     }
 
     private void OnTriggerEnter(Collider other)
