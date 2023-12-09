2f461ca48e907a0aaf7597660c65efd81a945879
sbseungbeom <sbseungbeom99@gmail.com>
2023-12-10 00:48:45 +0900

    썬라이트완성,골렘 스프라이트,애니 적용

--- Assets/Scripts/LimSeungBeom/FireGolem.cs
+++ Assets/Scripts/LimSeungBeom/FireGolem.cs
@@ -8,6 +8,15 @@ public class FireGolem : Enemy
     GameObject Player;
     [SerializeField] GameObject Warning;
 
+    [SerializeField] Sprite Jump;
+    [SerializeField] Sprite Fall;
+    [SerializeField] Sprite Idle;
+
+    [SerializeField] Sprite[] sprites = new Sprite[3];
+
+    SpriteRenderer spriter;
+    Animator anim;
+
     bool bAttackReady;
     bool bAttackStart;
     bool bReturn;
@@ -17,6 +26,9 @@ public class FireGolem : Enemy
     void Start()
     {
         Player = GameManager.Instance.Player.gameObject;
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
         Warning = transform.GetChild(0).gameObject;
         Warning.SetActive(false);
         
@@ -29,15 +41,19 @@ public class FireGolem : Enemy
         base.Update();
         if (bAttackReady )
         {
-            Rope.position = Vector3.MoveTowards(Rope.position, new Vector3(SavedEnemyPosition.x,SavedEnemyPosition.y + 20,SavedEnemyPosition.z), Time.deltaTime * 50);
+            Rope.position = Vector3.MoveTowards(Rope.position, new Vector3(SavedEnemyPosition.x,SavedEnemyPosition.y + 20,SavedEnemyPosition.z), Time.deltaTime * 70);
+
+            spriter.sprite = sprites[0];
         }
         if(bAttackStart )
         {
             Rope.position = Vector3.MoveTowards(Rope.position, new Vector3(SavedPlayerPosition.x,SavedPlayerPosition.y - 20,SavedPlayerPosition.z), Time.deltaTime * 50);
+            spriter.sprite = sprites[1];
         }
         if(bReturn)
         {
             Rope.position = Vector3.MoveTowards(Rope.position, new Vector3(SavedEnemyPosition.x, SavedEnemyPosition.y, SavedEnemyPosition.z), Time.deltaTime * 30);
+            spriter.sprite = sprites[2];
         }
 
         Warning.transform.position = new Vector3(SavedPlayerPosition.x, SavedPlayerPosition.y + 10, SavedPlayerPosition.z);
@@ -54,17 +70,35 @@ public class FireGolem : Enemy
         SavedEnemyPosition = new Vector3(Rope.position.x,Rope.position.y,Rope.position.z);
         SavedPlayerPosition = Player.transform.position;
 
+        anim.SetBool("AttackReady", true);
+
+        yield return new WaitForSeconds(2);
+        anim.SetBool("AttackReady", false);
+
+        //spriter.sprite = sprites[0];
         bAttackReady = true;
+
+
         yield return new WaitForSeconds(1);
+        //spriter.sprite = sprites[1];
+
         Warning.SetActive(true);
         Rope.position = new Vector3(SavedPlayerPosition.x, SavedPlayerPosition.y + 50, SavedPlayerPosition.z + 0.5f);
         bAttackReady = false;
+
+
         yield return new WaitForSeconds(2);
         Warning.SetActive(false);
         bAttackStart = true;
+
+
         yield return new WaitForSeconds(1);
+        //spriter.sprite = sprites[2];
+
         bAttackStart = false;
         bReturn = true;
+
+
         yield return new WaitForSeconds(3);
         bReturn = false;
 
